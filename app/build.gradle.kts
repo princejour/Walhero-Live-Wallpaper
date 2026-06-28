0c1f1ef391525a453de4ea7673fdf97ef6fe30a6
princejour <61734521+princejour@users.noreply.github.com>
2026-06-28 17:07:28 +0100

    Create installable test release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,15 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-val signingStoreFile = System.getenv("SIGNING_STORE_FILE")
-val signingStorePassword = System.getenv("SIGNING_STORE_PASSWORD")
-val signingKeyAlias = System.getenv("SIGNING_KEY_ALIAS")
-val signingKeyPassword = System.getenv("SIGNING_KEY_PASSWORD")
-val hasReleaseSigning = !signingStoreFile.isNullOrBlank() &&
-        !signingStorePassword.isNullOrBlank() &&
-        !signingKeyAlias.isNullOrBlank() &&
-        !signingKeyPassword.isNullOrBlank()
-
 android {
     namespace = "com.walhero.livewallpaper"
     compileSdk = 35
@@ -19,20 +10,9 @@ android {
     defaultConfig {
         applicationId = "com.walhero.livewallpaper"
         minSdk = 23
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
-    }
-
-    signingConfigs {
-        create("release") {
-            if (hasReleaseSigning) {
-                storeFile = file(signingStoreFile!!)
-                storePassword = signingStorePassword
-                keyAlias = signingKeyAlias
-                keyPassword = signingKeyPassword
-            }
-        }
+        targetSdk = 34
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
@@ -41,14 +21,13 @@ android {
             versionNameSuffix = "-debug"
         }
         release {
+            isDebuggable = false
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            if (hasReleaseSigning) {
-                signingConfig = signingConfigs.getByName("release")
-            }
         }
     }
 }
